20a16d73c3eb4ae6e107bc4903351d3b68b49d52
seunghun2 <wsh9991@gmail.com>
2025-12-28 23:38:25 +0900

    fix: 충돌하는 외부 CSS 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,8 +80,6 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="/css/kakao-style.css" />
-        <link rel="stylesheet" href="/css/template-select.css" />
       </head>
       <body suppressHydrationWarning>
         <MantineProvider theme={theme} defaultColorScheme="light">
