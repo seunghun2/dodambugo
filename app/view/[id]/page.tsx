643a8a474e04caf95176dc120be544a294f9983d
seunghun2 <wsh9991@gmail.com>
2026-01-04 10:53:26 +0900

    카카오 이미지 v3로 변경 (캐시 버스팅)

--- app/view/[id]/page.tsx
+++ app/view/[id]/page.tsx
@@ -175,7 +175,7 @@ export default function ViewPage() {
                     description: bugo?.funeral_home
                         ? `${bugo.funeral_home}${bugo.room_number ? ' ' + bugo.room_number : ''}`
                         : '',
-                    imageUrl: 'https://dodambugo.com/og-bugo-v2.png',
+                    imageUrl: 'https://dodambugo.com/og-bugo-v3.png',
                     link: { mobileWebUrl: url, webUrl: url }
                 },
                 buttons: [{ title: '부고 확인하기', link: { mobileWebUrl: url, webUrl: url } }]
