3c136c8fa0dec774ab14077ac89341adc2da2ec4
seunghun2 <wsh9991@gmail.com>
2026-01-09 15:20:33 +0900

    refactor(layout): MainLayout 분리 및 View 페이지 독립성 확보

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/c
 import { Notifications } from '@mantine/notifications';
 import { ModalsProvider } from '@mantine/modals';
 import { dodamTheme } from '@/lib/theme';
-import MainLayout from '@/components/MainLayout';
 import KakaoInit from '@/components/KakaoInit';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
 
@@ -79,9 +78,8 @@ export default function RootLayout({
         <MantineProvider theme={dodamTheme} defaultColorScheme="light">
           <ModalsProvider>
             <Notifications position="top-right" />
-            <MainLayout>
-              {children}
-            </MainLayout>
+            <Notifications position="top-right" />
+            {children}
           </ModalsProvider>
         </MantineProvider>
       </body>
