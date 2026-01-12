68b72b086559badf932bfca9316a45d2d3c6957b
seunghun2 <wsh9991@gmail.com>
2026-01-12 12:30:22 +0900

    fix: iOS 주소/전화번호 자동 링크 비활성화

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
     <html lang="ko" {...mantineHtmlProps}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta name="naver-site-verification" content="4f09b03be58e139284a268de39527d36c7439898" />
         <meta name="google-site-verification" content="19Py1zFue07o3TzDBzUlkuiJ_D7fwRBOqh44i21eK10" />
         <ColorSchemeScript defaultColorScheme="light" />
